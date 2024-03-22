b2631ed4e283b77384f0bd034685d22a4cf7f1f7
Vid0mina <0valentine0u0@gmail.com>
2024-03-22 12:29:26 +0300

    hw9

--- src/main/java/ru/ushakova/java/basic/hws/Application.java
+++ src/main/java/ru/ushakova/java/basic/hws/Application.java
@@ -1,7 +1,30 @@
 package ru.ushakova.java.basic.hws;
 
+import java.util.*;
+
 public class Application {
+
     public static void main(String[] args) {
 
+        List<Integer> list = new ArrayList<>(List.of(1,3,5,9,1));
+        sum(list);
+
+
     }
+
+//    public List getMinMax(Integer min, Integer max){
+//
+//    }
+
+    public static Integer sum(List list){
+        ListIterator<Integer> listIterator = list.listIterator();
+
+        while (listIterator.hasNext()) {
+            Integer integer = listIterator.next();
+            System.out.println(integer);
+        }
+        Integer sum = 0;
+        return sum;
+    }
+
 }
