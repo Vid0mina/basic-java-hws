7d53752f3ce7992fbb0a15e0cf8739318648d1f8
Valentina U. <tech.profile.acc@gmail.com>
2024-04-16 22:28:28 +0300

    hw21_Основы многопоточности_Часть 2

--- src/main/java/ru/ushakova/java/basic/hws/Application.java
+++ src/main/java/ru/ushakova/java/basic/hws/Application.java
@@ -1,7 +1,25 @@
 package ru.ushakova.java.basic.hws;
 
-public class Application {
-    public static void main(String[] args) {
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class Application {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        Printer printer = new Printer();
+
+        Priority priority1 = new Priority(1, printer);
+        Priority priority2 = new Priority(2, printer);
+        Priority priority3 = new Priority(3, printer);
+
+        executorService.submit(priority1);
+        executorService.submit(priority2);
+        executorService.submit(priority3);
+
+        executorService.shutdown();
     }
+
 }
